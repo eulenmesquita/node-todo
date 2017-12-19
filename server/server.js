f3d75992466e0cdbcb0d4fa19064b3bbf5dfc82b
Eulen Mesquita <eulen.mesquita@gmail.com>
2017-12-20 09:45:00 +1000

    Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -34,6 +36,25 @@ app.get('/todos', (req, res)=> {
             .send(e);
     });
 });
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send({});
+    }
+
+    Todo.findById(id)
+        .then((todo) => {
+            if (todo) {
+                return res.status(200).send({todo});
+            }
+            res.status(404).send({});
+        })
+        .catch((e) => {
+            res.status(400).send({});
+        })
+    }); 
 
 app.listen(3000, () => {
     console.log(`Started on port ${port}`);
