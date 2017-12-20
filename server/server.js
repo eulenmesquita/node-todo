e2d4f4d8190a99fcf988c4e38075676c61132a1d
Eulen Mesquita <eulen.mesquita@gmail.com>
2017-12-20 10:00:04 +1000

    Fix conflicts

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.post('/todos', (req, res) => {
         console.log('new Todo saved');
     }, (e) => {
         res.status(400).send(e);
-    })
+    });
 });
 
 app.get('/todos', (req, res)=> {
@@ -41,45 +41,19 @@ app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
     
     if (!ObjectID.isValid(id)) {
-<<<<<<< HEAD
         return res.status(404).send({});
-=======
-<<<<<<< HEAD
-        return res.status(404).send({});
-=======
-        res.status(404).send({});
->>>>>>> 5022d1a254cf29862271c91ea1be44b428060058
->>>>>>> e641db92917afd7b22ef4d0fb8ef4b00ba54bbdf
     }
 
-    Todo.findById(id)
-        .then((todo) => {
-            if (todo) {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> e641db92917afd7b22ef4d0fb8ef4b00ba54bbdf
-                return res.status(200).send({todo});
-            }
-            res.status(404).send({});
-        })
-        .catch((e) => {
-            res.status(400).send({});
-        })
-    }); 
-<<<<<<< HEAD
-=======
-=======
-                res.status(200).send({todo});
-            } else {
-                res.status(404).send({});
-            }
-        }, (e) => {
-            res.status(400).send({});
-        });
-});
->>>>>>> 5022d1a254cf29862271c91ea1be44b428060058
->>>>>>> e641db92917afd7b22ef4d0fb8ef4b00ba54bbdf
+    Todo.findById(id).then((todo) => {
+        if (todo) {
+            return res.status(200).send({todo});
+        }
+        res.status(404).send({});
+    })
+    .catch((e) => {
+        res.status(400).send({});
+    });
+}); 
 
 app.listen(3000, () => {
     console.log(`Started on port ${port}`);
