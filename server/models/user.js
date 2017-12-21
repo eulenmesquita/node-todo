894a5906641b841b56db8747621ba5a47588c55d
eulen@minicode.com.au <eulen@minicode.com.au>
2017-12-21 15:55:23 +1000

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('Users', {
+var userSchema = new mongoose.Schema({
     name:{
         type: String,
         minlength: 4,
@@ -44,5 +46,31 @@ var User = mongoose.model('Users', {
         default: null    
     }
 });
+
+userSchema.methods.toJSON = function() {
+
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject, ['_id','email','name']);
+}
+
+userSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens.push({
+        access,
+        token
+    });
+
+    return user.save().then(() => {
+        return token
+    });
+    
+}
+
+var User = mongoose.model('Users', userSchema);
 
 module.exports = { User }
